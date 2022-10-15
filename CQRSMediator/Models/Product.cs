314a53e418c4cbe0f27a1ea8c085fafb4ad2f2c7
RakeshPattanayak56 <rakeshpattanayak56@gmail.com>
2022-10-15 22:22:50 +0530

    New CQRS updated code

---
+++ CQRSMediator/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSMediator.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+        public string Price { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNo { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# EmployeePortalApi.CQRS
+# ASP.NET Core CRUD Operation with CQRS and MediatR Patterns
+
+Here we will Implementing ASP.NET Core CRUD Operation with CQRS and MediatR Patterns.
+
